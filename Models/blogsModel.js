217f2eb6ce6d77bd0bd2dc60c42259dd35f3bfd3
Harsh25081 <harsh.pataudi258@gmail.com>
2022-11-14 12:52:06 +0530

    added blogModel Schema

---
+++ Models/blogsModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+const blogsSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    body : {
+        type : String,
+        required : true
+    },
+    authorId : {
+        type : ObjectId,
+        required : true,
+        ref : //to be given
+    },
+    tags : {
+        type : Array,
+        default : []
+    },
+    category : {
+        type : String,
+        required : true
+    },
+    subcategory : {
+        type : Array,
+        default : []
+    },
+    isDeleted : {
+        type : Boolean,
+        default : false
+    },
+    isPublished : {
+        type : Boolean,
+        default : false
+    }
+},{timettamps : true})
+
+module.exports = mongoose.model("BlogsModel",blogsSchema)
